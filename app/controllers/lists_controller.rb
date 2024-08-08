ea8306b36cf97be65288f53521113a0f7d605ca8
Léo Turcat <turcatleo@gmail.com>
2024-08-08 18:48:10 +0200

    exo2

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,42 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: [:show]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
